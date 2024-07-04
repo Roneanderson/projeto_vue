4a3cf89d3aeebd16867e0e119d1720ad8935cc2c
Roneanderson <roneanderson496@gmail.com>
2024-07-04 12:01:06 -0300

    em andamento de codigo

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,81 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive, } from 'vue';
+
+const estado = reactive({
+    tarefa: [
+        {
+            numero: 0,
+            numero2: 0,
+            resultado: 0,
+            operador: 'somar',
+        }
+    ]
+})
+
+function getNumber() {
+    console.log(estado.numero)
+    if (estado.numero == '') {
+        estado.numero = 0;
+    }
+    estado.numero = parseFloat(estado.numero)
+    getOperacao()
+}
+
+function getNumber2() {
+    if (estado.numero2 == '') {
+        estado.numero2 = 0;
+    }
+    estado.numero = parseFloat(estado.numero)
+    getOperacao()
+}
+
+const getOperacao = () => {
+    switch(estado.operador) {
+        case 'adicao':
+            return estado.tarefa = (estado.numero + estado.numero2);
+    }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col">
+                    <input  @change="evento => getNumber = evento.target.value" type="number" placeholder="Digite valor 1 ">
+                </div>
+                <div class="col-md-3">
+                    <ul>
+                        <select @change="evento => estado.operador = evento.target.value " class="col-md-3 p-md-1">
+                            <option value="adicao">+</option>
+                            <option value="subtracao">-</option>
+                            <option value="multiplicacao">*</option>
+                            <option value="divisao">/</option>
+                        </select>
+                    </ul>
+                </div>
+                <div class="col">
+                    <input @change="evento => getNumber2 = evento.target.value" type="number" placeholder="Digite valor 2">
+                </div>
+                
+                <div class="col">
+                    <input @change="evento => getOperacao = evento.target.value" type="number" placeholder="resultado">
+                </div>
+            </div>
+        </form>
+        <ul class="list-group mt-4">
+            <li class="list-group-item" v-for="tarefas in estado.tarefa">
+                <input :checked="tarefas.resultado" :id="tarefas.resultado" type="checkbox">
+                <label :class="{done: tarefas.resultado}" class="ms-3" :for="tarefas.resultado">
+                    {{ tarefas.resultado }}
+                </label>
+            </li>
+        </ul>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.done {
+    text-decoration: line-through;
 }
 </style>
